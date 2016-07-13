d2b069acf9e16df99f9e874b4a13068e3ef07253
Ink1804 <keksman89@gmail.com>
2016-07-13 20:52:03 +0300

    updated design

--- app/src/main/java/com/example/ink1804/smol_gazeta/MainActivity.java
+++ app/src/main/java/com/example/ink1804/smol_gazeta/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.ink1804.smol_gazeta;
 
 import android.content.Context;
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.ink1804.smol_gazeta.adapters.ViewPagerAdapter;
 import com.example.ink1804.smol_gazeta.fragments.SingleNews_Fragment;
@@ -18,10 +25,9 @@ import com.example.ink1804.smol_gazeta.fragments.SingleNews_Fragment;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
         SingleNews_Fragment.OnCreateFragmentListener{
 
-    String[] subStrings,keyStrings;
     private ViewPagerAdapter mPagerAdapter;
     private ViewPager mViewPager;
-
+    DrawerLayout drawer;
     private RecyclerView recycler_hotNews;
     private HtmlParser parser;
     private String gazetaURL = "http://www.smolgazeta.ru";
@@ -30,26 +36,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        mViewPager = (ViewPager) findViewById(R.id.subMenu_pager);
-        setViewPagerAdapter(R.array.main_subs,R.array.main_keys);
-
-        recycler_hotNews = (RecyclerView)findViewById(R.id.recycle_hot_news);
-        recycler_hotNews.setLayoutManager(new LinearLayoutManager(this));
-        LinearLayoutManager horizontalLayoutManager
-                = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
-        recycler_hotNews.setLayoutManager(horizontalLayoutManager);
-
-        parser = new HtmlParser();
-        parser.parseHotNews(gazetaURL,getApplicationContext(),recycler_hotNews);
+//reclame
+        createReclameBlock();
+        createNavigationDrawer();
+        createRecyclerViews();
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
+
         switch (id){
             case R.id.nav_main:
                 setViewPagerAdapter(R.array.main_subs,R.array.main_keys);
@@ -72,11 +68,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_documents:
                 setViewPagerAdapter(R.array.documents_subs,R.array.documents_keys);
                 break;
+            default:break;
 //            case R.id.nav_poster:
 //                setViewPagerAdapter(R.array.poster_subs,R.array.poster_keys);
 //                break;
         }
-        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.main_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
@@ -85,12 +81,62 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public Context getContext()  {
         return this;
     }
-    private void setViewPagerAdapter(int subStrings,int keyStrings){
+
+    private void setViewPagerAdapter(int subStrings, int keyStrings){
         String[] subs,keys;
         subs = getResources().getStringArray(subStrings);
         keys = getResources().getStringArray(keyStrings);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
         mPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), subs, keys);
         mViewPager.setAdapter(mPagerAdapter);
+        tabLayout.setupWithViewPager(mViewPager);
+        for(int i=0;i<subs.length;i++){
+            tabLayout.getTabAt(i).setText(subs[i]);
+        }
     }
+    private void createNavigationDrawer(){
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.theme_main, R.string.theme_poster);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    private void createRecyclerViews(){
+        mViewPager = (ViewPager) findViewById(R.id.subMenu_pager);
+        setViewPagerAdapter(R.array.main_subs,R.array.main_keys);
+
+        recycler_hotNews = (RecyclerView)findViewById(R.id.recycle_hot_news);
+        recycler_hotNews.setLayoutManager(new LinearLayoutManager(this));
+
+        parser = new HtmlParser();
+        parser.parseHotNews(gazetaURL,getApplicationContext(),recycler_hotNews);
+        //        LinearLayoutManager horizontalLayoutManager
+//                = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+//        recycler_hotNews.setLayoutManager(horizontalLayoutManager);
+    }
+    private void createReclameBlock(){
+        ImageView block_one = (ImageView)findViewById(R.id.imView_reclame1);
+        ImageView block_two = (ImageView)findViewById(R.id.imView_reclame2);
+        block_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ReclameActivity.class);
+                intent.putExtra("url","http://fizmat.smolgu.ru");
+                startActivity(intent);
+            }
+        });
+        block_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ReclameActivity.class);
+                intent.putExtra("url","https://gymnastics.competitions.center");
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/ink1804/smol_gazeta/adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/example/ink1804/smol_gazeta/adapters/ViewPagerAdapter.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
